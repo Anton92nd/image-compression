7164eab266dd1d4846b58c203a8f0c80299b19eb
Anton92nd <anton92nd@gmail.com>
2016-10-09 14:55:06 +0500

    Visual Studio project created

---
+++ ImageCompression/ImageCompression/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace ImageCompression
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Left_Open_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuItem_Left_Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuItem_Right_Open_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuItem_Right_Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
