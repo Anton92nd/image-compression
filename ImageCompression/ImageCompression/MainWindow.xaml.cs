f0f4a407a44fa7f81b5f160ecebf5dba0702ed8b
Anton92nd <anton92nd@gmail.com>
2016-10-10 12:51:39 +0500

    Calculating PSNR

--- ImageCompression/ImageCompression/MainWindow.xaml.cs
+++ ImageCompression/ImageCompression/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
@@ -20,6 +21,7 @@ namespace ImageCompression
     {
         public MainWindow()
         {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             InitializeComponent();
         }
 
@@ -142,6 +144,42 @@ namespace ImageCompression
             LeftImageBox.Source = BitmapSource.Create(bitmap.PixelWidth, bitmap.PixelHeight, bitmap.DpiX, bitmap.DpiY, bitmap.Format,
                 null, bytes, bitmap.PixelWidth * (bitmap.Format.BitsPerPixel / 8));
             ButtonApplyLeft.IsEnabled = true;
+        }
+
+        private void ButtonPSNR_OnClick(object sender, RoutedEventArgs e)
+        {
+            var leftImage = LeftImageBox.Source as BitmapSource;
+            var rightImage = RightImageBox.Source as BitmapSource;
+            if (leftImage == null || rightImage == null)
+                return;
+            var bytesLeft = leftImage.GetBytes();
+            var bytesRight = rightImage.GetBytes();
+            if (bytesLeft.Length != bytesRight.Length || leftImage.PixelWidth != rightImage.PixelWidth
+                || leftImage.PixelHeight != rightImage.PixelHeight)
+            {
+                MessageBox.Show("Images have different format", "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+            var sum = 0.0;
+            for (var i = 0; i < bytesLeft.Length; ++i)
+            {
+                sum += Sqr(bytesLeft[i] - bytesRight[i]);
+            }
+            var PSNR = Math.Abs(sum) < 1e-5 ? double.PositiveInfinity : 10.0*Math.Log10(Sqr(255)*leftImage.PixelHeight*leftImage.PixelWidth/sum);
+            MessageBox.Show(string.Format("PSNR: {0:0.00####}", PSNR), "PSNR", MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private double Sqr(double x)
+        {
+            return x*x;
+        }
+
+        private void ButtonApply_OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (ButtonApplyLeft.IsEnabled && ButtonApplyRight.IsEnabled)
+                ButtonPSNR.IsEnabled = true;
         }
     }
 }
