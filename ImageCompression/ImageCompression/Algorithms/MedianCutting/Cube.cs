1580563fd68b96afe33651473d51ad23f5c4747a
Anton92nd <anton92nd@gmail.com>
2016-10-17 23:35:44 +0500

    fixed median cut

--- ImageCompression/ImageCompression/Algorithms/MedianCutting/Cube.cs
+++ ImageCompression/ImageCompression/Algorithms/MedianCutting/Cube.cs
@@ -1,31 +1,106 @@
-﻿using ImageCompression.Structures;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ImageCompression.Structures;
 using JetBrains.Annotations;
 
 namespace ImageCompression.Algorithms.MedianCutting
 {
-    public struct Cube
+    public class Cube
     {
-        public Cube(Segment ox, Segment oy, Segment oz)
+        public Vector<byte>[] Vectors { get; private set; }
+
+        public Cube(IEnumerable<Vector<byte>> vectors)
         {
-            Ox = ox;
-            Oy = oy;
-            Oz = oz;
+            Vectors = vectors.ToArray();
         }
 
-        [System.Diagnostics.Contracts.Pure]
-        public bool Contains(Vector<byte> color)
+        public void Split(out Cube cube1, out Cube cube2)
         {
-            return Ox.Start <= color[0] && color[0] <= Ox.End &&
-                   Oy.Start <= color[1] && color[1] <= Oy.End &&
-                   Oz.Start <= color[2] && color[2] <= Oz.End;
+            byte[] min, max;
+            var stats = GetStats(out min, out max);
+            for (var i = 0; i < 3; i++)
+            {
+                var j = (i + 1)%3;
+                var k = (i + 2)%3;
+                if (max[i] - min[i] >= max[j] - min[j] && max[i] - min[i] >= max[k] - min[k])
+                {
+                    var index = BinarySearch(stats[i], min[i], max[i]);
+                    SplitByIndex(i, index, Vectors, out cube1, out cube2);
+                    return;
+                }
+            }
+            throw new Exception("Impossible exception");
         }
 
-        [NotNull]
-        public override string ToString()
+        private int[][] GetStats(out byte[] min, out byte[] max)
         {
-            return string.Format("<{0}, {1}, {2}>", Ox, Oy, Oz);
+            var stats = new int[3][];
+            stats[0] = new int[256];
+            stats[1] = new int[256];
+            stats[2] = new int[256];
+            min = new byte[] {255, 255, 255};
+            max = new byte[] {0, 0, 0};
+            foreach (var vector in Vectors)
+            {
+                for (var i = 0; i < 3; i++)
+                {
+                    min[i] = Math.Min(min[i], vector[i]);
+                    max[i] = Math.Max(max[i], vector[i]);
+                    stats[i][vector[i]]++;
+                }
+            }
+            for (var i = 0; i < 3; ++i)
+            {
+                for (var j = 1; j < 256; ++j)
+                    stats[i][j] += stats[i][j - 1];
+            }
+            return stats;
         }
 
-        public readonly Segment Ox, Oy, Oz;
+        public Vector<byte> GetMedian()
+        {
+            byte[] min, max;
+            var stats = GetStats(out min, out max);
+            var v = new byte[]{0, 0, 0};
+            for (var i = 0; i < 3; i++)
+            {
+                v[i] = (byte) BinarySearch(stats[i], min[i], max[i]);
+            }
+            return new Vector<byte>(v);
+        }
+
+        private static void SplitByIndex(int dim, int splitBy, Vector<byte>[] vectors, out Cube cube1, out Cube cube2)
+        {
+            var list1 = new List<Vector<byte>>();
+            var list2 = new List<Vector<byte>>();
+            foreach (var vector in vectors)
+            {
+                if (vector[dim] <= splitBy)
+                {
+                    list1.Add(vector);
+                }
+                else
+                {
+                    list2.Add(vector);
+                }
+            }
+            cube1 = new Cube(list1);
+            cube2 = new Cube(list2);
+        }
+
+        private static int BinarySearch([NotNull] int[] array, int l, int r)
+        {
+            var toFind = (array[l] + array[r])/2;
+            while (l + 1 < r)
+            {
+                var m = (l + r)/2;
+                if (array[m] < toFind)
+                    l = m;
+                else
+                    r = m;
+            }
+            return l;
+        }
     }
 }
--- ImageCompression/ImageCompression/Algorithms/MedianCutting/MedianCut.cs
+++ ImageCompression/ImageCompression/Algorithms/MedianCutting/MedianCut.cs
@@ -9,115 +9,36 @@ namespace ImageCompression.Algorithms.MedianCutting
     public static class MedianCut
     {
         [NotNull]
-        public static Vector<byte>[] Build(int palletSize, [NotNull] IList<Vector<byte>> colors, out Vector<byte>[] pallet)
+        public static Vector<byte>[] Build(int paletteSize, [NotNull] IList<Vector<byte>> colors, out Vector<byte>[] palette)
         {
-            if ((palletSize & (palletSize - 1)) != 0)
+            if ((paletteSize & (paletteSize - 1)) != 0)
                 throw new Exception("Pallet size must be power of 2");
-            Cube initialCube;
-            var stats = GetStatistics(colors, out initialCube);
-            var root = new MCNode(initialCube);
-            var leaves = new MCNode[]{root};
-            while (leaves.Length < palletSize)
+            var cubes = new List<Cube> {new Cube(colors)};
+            while (cubes.Count < paletteSize)
             {
-                var newLeaves = new List<MCNode>();
-                foreach (var node in leaves)
+                var newCubes = new List<Cube>();
+                foreach (var cube in cubes)
                 {
                     Cube cube1, cube2;
-                    SplitCube(stats, node.Cube, out cube1, out cube2);
-                    var l = new MCNode(cube1);
-                    var r = new MCNode(cube2);
-                    node.L = l;
-                    node.R = r;
-                    newLeaves.Add(l);
-                    newLeaves.Add(r);
+                    cube.Split(out cube1, out cube2);
+                    newCubes.Add(cube1);
+                    newCubes.Add(cube2);
                 }
-                leaves = newLeaves.ToArray();
+                cubes = newCubes;
             }
-            pallet = leaves.Select(l => GetCubeMedian(stats, l.Cube)).ToArray();
-            return colors.Select(color => FindColor(stats, root, color)).ToArray();
-        }
-
-        private static Vector<byte> FindColor([NotNull] int[][] stats, [NotNull] MCNode mcNode, Vector<byte> color)
-        {
-            if (mcNode.L == null)
-                return GetCubeMedian(stats, mcNode.Cube);
-            if (mcNode.L.Cube.Contains(color))
-                return FindColor(stats, mcNode.L, color);
-            return FindColor(stats, mcNode.R, color);
-        }
-
-        private static Vector<byte> GetCubeMedian([NotNull] int[][] stats, Cube cube)
-        {
-            var indexR = BinarySearch(stats[0], cube.Ox);
-            var indexG = BinarySearch(stats[1], cube.Oy);
-            var indexB = BinarySearch(stats[2], cube.Oy);
-            return new Vector<byte>(new []{(byte)indexR, (byte)indexG, (byte)indexB});
-        }
-
-        private static int BinarySearch([NotNull] int[] array, Segment segment)
-        {
-            int l = segment.Start, r = segment.End;
-            var toFind = (array[l] + array[r])/2;
-            while (l + 1 < r)
+            var dictPalette = new Dictionary<Vector<byte>, Vector<byte>>();
+            var listPalette = new List<Vector<byte>>();
+            foreach (var cube in cubes)
             {
-                var m = (l + r)/2;
-                if (array[m] < toFind)
-                    l = m;
-                else
-                    r = m;
-            }
-            return l;
-        }
-
-        private static void SplitCube([NotNull] int[][] stats, Cube cube, out Cube cube1, out Cube cube2)
-        {
-            if (cube.Ox.Length >= cube.Oy.Length && cube.Ox.Length >= cube.Oz.Length)
-            {
-                var index = BinarySearch(stats[0], cube.Ox);
-                cube1 = new Cube(new Segment(cube.Ox.Start, index), cube.Oy, cube.Oz);
-                cube2 = new Cube(new Segment(index + 1, cube.Ox.End), cube.Oy, cube.Oz);
-            }
-            else if (cube.Oy.Length >= cube.Ox.Length && cube.Oy.Length >= cube.Oz.Length)
-            {
-                var index = BinarySearch(stats[1], cube.Oy);
-                cube1 = new Cube(cube.Ox, new Segment(cube.Oy.Start, index), cube.Oz);
-                cube2 = new Cube(cube.Ox, new Segment(index + 1, cube.Oy.End), cube.Oz);
-            }
-            else
-            {
-                var index = BinarySearch(stats[2], cube.Oz);
-                cube1 = new Cube(cube.Ox, cube.Oy, new Segment(cube.Oz.Start, index));
-                cube2 = new Cube(cube.Ox, cube.Oy, new Segment(index + 1, cube.Oz.End));
-            }
-        }
-
-        [NotNull]
-        private static int[][] GetStatistics([NotNull] IList<Vector<byte>> colors, out Cube cube)
-        {
-            var counts = new int[3][];
-            counts[0] = new int[256];
-            counts[1] = new int[256];
-            counts[2] = new int[256];
-            var mins = new int[3] {300, 300, 300};
-            var maxs = new int[3] {0, 0, 0};
-            foreach (var vector in colors)
-            {
-                for (var i = 0; i < 3; ++i)
+                var value = cube.GetMedian();
+                listPalette.Add(value);
+                foreach (var vector in cube.Vectors)
                 {
-                    counts[i][vector[i]]++;
-                    maxs[i] = Math.Max(maxs[i], vector[i]);
-                    mins[i] = Math.Min(mins[i], vector[i]);
+                    dictPalette[vector] = value;
                 }
             }
-            cube = new Cube(new Segment(mins[0], maxs[0]), new Segment(mins[1], maxs[1]), new Segment(mins[2], maxs[2]));
-            for (var i = 0; i < 3; i++)
-            {
-                for (var j = 1; j < 256; j++)
-                {
-                    counts[i][j] += counts[i][j - 1];
-                }
-            }
-            return counts;
+            palette = listPalette.ToArray();
+            return colors.Select(c => dictPalette[c]).ToArray();
         }
     }
 }
